96c8b57d550dad2282626bbb94f6854cc53f1050
vcontrer4s <vikmacon@gmail.com>
2020-05-16 17:53:19 -0400

    inicio del proyecto svelte-webpack

---
+++ src/containers/App.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,300&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+    :global(body) {
+        background-color: #fafafa;
+        color: rgba(38, 38, 38, 0.7);
+        font-family: "Lato" sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+    :global(h1, h2, h3) {
+        margin: 0;
+        padding: 0; 
+    }
+
+</style>
